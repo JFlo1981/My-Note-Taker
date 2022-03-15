06cf7095a697fe934745351902ad43b17c796cb7
Jarod Florence <jarod.florence@outlook.com>
2022-03-15 12:25:44 -0500

    HTML routes

---
+++ routes/HTMLroutes.js
@@ -0,0 +1,18 @@
+const route1 = require('express').Router();
+
+const path = require("path")
+
+
+route1.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+route1.get("/notes",function(req,res){
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+})
+
+route1.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+module.exports = route1;
